616b5d09da9e588479800f6d05b352ebde9c7e5c
Anna Kopeikina <karavay3@gmail.com>
2022-10-16 20:13:42 +0300

    add API

---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,20 @@
+package ru.netology;
+
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 03;
+        post.birthday.year = 1999;
+        post.name = "Иван";
+        post.passport = "7000564356";
+        post.patronymic = "Иванович";
+        post.phone = "+79000000000";
+        post.surname = "Иванов";
+        post.subscribe = true;
+
+    }
+
+
+}
